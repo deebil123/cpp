aef731c0f2e67e2b2146a21e3d602fd59203d730
deebil123 <55847447+deebil123@users.noreply.github.com>
2019-09-26 11:51:35 -0400

    Create the_setup.cpp

---
+++ the_setup.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+  string input;
+cout << " WASUP " << "\n";
+cout << "What of the week is it ?" << endl;
+cin >> input;
+cout << "your answer" << " " << input << endl;
+
+
+
+return 0;  
+}
